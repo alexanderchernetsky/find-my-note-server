6039cccb2337c5465fbca3049f3f1a904f9ccfe1
Alex Chernetsky <a.chernetsky@itransition.com>
2022-05-22 21:37:50 +0100

    Refactor /login route

--- routes/login.js
+++ routes/login.js
@@ -54,40 +54,42 @@ loginRoutes.post("/login", validateResourceMW(validator.loginSchema), (req, res)
         .then(user => {
             // if user does not exist then return status 400
             if (!user) {
+                logger.log(logTypes.ERROR, "User with such email does not exist!");
                 return res.status(400).json({ message: "User with such email does not exist!" })
+            } else {
+                // if user exist - compares passwords
+                // 'password' comes from the FE request
+                // 'user.password' comes from the database
+                bcrypt.compare(password, user.password, (error, data) => {
+                    if (error) {
+                        logger.log(logTypes.ERROR, `bcrypt error, ${error}`);
+                        throw error;
+                    }
+                    // if both match than you can do anything
+                    if (data) {
+                        const token = jwt.sign({
+                            email,
+                            password
+                        }, process.env.JWT_SECRET, {
+                            expiresIn: 60 * 60 * 2 // seconds*minutes*hours, 2 hours in this case
+                        }); // JWT is way for securely transmitting information between parties as a JSON object. This information can be verified and trusted because it is digitally signed.
+
+                        logger.log(logTypes.INFO, `Logged in successfully! User: ${user.user_name}. Email: ${user.email}`);
+                        return res
+                            .cookie("access_token", token, {
+                                httpOnly: true, // the httpOnly flag ensures that no client-side script can access the cookie other than the server.
+                                secure: process.env.NODE_ENV === "production", // The secure flag ensures that cookie information is sent to the server with an encrypted request over the HTTPS protocol.
+                                sameSite: "none"
+                            })
+                            .status(200)
+                            .json({message: "Logged in successfully!", user: {email: user.email, user_name: user.user_name, id: user._id}});
+
+                    } else {
+                        logger.log(logTypes.ERROR, 'Failed to login. Invalid credentials!');
+                        return res.status(401).json({message: "Invalid credentials!"});
+                    }
+                })
             }
-            // if user exist - compares passwords
-            // 'password' comes from the FE request
-            // 'user.password' comes from the database
-            bcrypt.compare(password, user.password, (error, data) => {
-                if (error) {
-                    logger.log(logTypes.ERROR, `bcrypt error, ${error}`);
-                    throw error;
-                }
-                // if both match than you can do anything
-                if (data) {
-                    const token = jwt.sign({
-                        email,
-                        password
-                    }, process.env.JWT_SECRET, {
-                        expiresIn: 60 * 60 * 2 // seconds*minutes*hours, 2 hours in this case
-                    }); // JWT is way for securely transmitting information between parties as a JSON object. This information can be verified and trusted because it is digitally signed.
-
-                    logger.log(logTypes.INFO, `Logged in successfully! User: ${user.user_name}. Email: ${user.email}`);
-                    return res
-                        .cookie("access_token", token, {
-                            httpOnly: true, // the httpOnly flag ensures that no client-side script can access the cookie other than the server.
-                            secure: process.env.NODE_ENV === "production", // The secure flag ensures that cookie information is sent to the server with an encrypted request over the HTTPS protocol.
-                            sameSite: "none"
-                        })
-                        .status(200)
-                        .json({message: "Logged in successfully!", user: {email: user.email, user_name: user.user_name, id: user._id}});
-
-                } else {
-                    logger.log(logTypes.ERROR, 'Failed to login. Invalid credentials!');
-                    return res.status(401).json({message: "Invalid credentials!"});
-                }
-            })
         });
 });
 
